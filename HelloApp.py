dedc1373beaf6edabddbd9a9edb1c16d39760537
pank-su <frostkslo1@yandex.ru>
2023-02-22 10:30:26 +0300

    Added my task

--- HelloApp.py
+++ HelloApp.py
@@ -3,3 +3,9 @@ s: str = "*" * 30
 print(s)
 print("New project")
 print("")
+
+import cProfile
+import re
+
+r = re.compile("\\d\\S")
+cProfile.run("""[r.findall("sdfdsfD, 1d, 7f") for i in range(1000000)]""")
